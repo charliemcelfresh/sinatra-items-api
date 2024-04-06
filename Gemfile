0cddeee1709327a4e38bcef53366219d35f87ea9
Charlie McElfresh <cwmcelfresh@gmail.com>
2024-04-05 17:24:05 -0700

    Add some serial vs concurrent endpoints

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "3.3.0"
 gem 'dotenv', '~> 3.1.0'
 gem 'mysql2', '~> 0.5.6'
 gem 'oj', '~> 3.16', '>= 3.16.3'
+gem 'parallel', "~> 1.24.0"
 gem "puma", ">= 5.0"
 gem 'sequel', '~> 5.78.0'
 gem "sinatra", "~> 4.0.0"
--- my_app.rb
+++ my_app.rb
@@ -1,10 +1,11 @@
 require 'sinatra/base'
 require 'sequel'
 require 'dotenv/load'
+require 'parallel'
 
 class MyApp < Sinatra::Base
     before do
-        pass if request.path_info == '/api/v1/user_ids'
+        pass if request.path_info == '/api/v1/user_ids' || request.path_info == 'api/v1/serial'
         get_user_id
     end
 
@@ -15,7 +16,7 @@ class MyApp < Sinatra::Base
     DB = Sequel.connect(ENV['DB_URL'])
 
     get '/api/v1/items' do
-        page = params[:page].to_i || 0
+        limit, offset = params[:limit].to_i || 10, params[:offset].to_i || 0
         statement = %q(
             SELECT
                 i.id, i.name, i.created_at, i.updated_at
@@ -25,16 +26,32 @@ class MyApp < Sinatra::Base
                 user_items ui ON ui.item_id = i.id
             WHERE
                 ui.user_id = ?
-            LIMIT 10
+            LIMIT ?
             OFFSET ?
         )
-        items = DB.fetch(statement, @user_id, page)
-        return items.all.to_json
+        items = DB.fetch(statement, @user_id, limit, offset)
+        items.all.to_json
+    end
+
+    get '/api/v1/serial' do
+        start = Time.now
+        [1,2,3].each do |n|
+            sleep(n)
+        end
+        {"elapsed": Time.now - start}.to_json
+    end
+
+    get '/api/v1/concurrent' do
+        start = Time.now
+        results = Parallel.map((0..3).map.to_a) do |n|
+            sleep(n)
+        end
+        {"elapsed": Time.now - start}.to_json
     end
 
     get '/api/v1/user_ids' do
         user_ids = DB[:users].select(:id).order(:id).all
-        return user_ids.to_json
+        user_ids.to_json
     end
 
     def get_user_id
